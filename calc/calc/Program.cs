33ccbb24392a137bc01f5b587c43b31fdc8418e9
thinteen <80061280+thinteen@users.noreply.github.com>
2022-03-29 23:08:50 +0300

    Add files via upload

---
+++ calc/calc/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace calc
+{
+    class Program
+    {
+        private  static double Sum(double val1, double val2)
+        {
+            return val1 + val2;
+        }
+
+        private static double Subtraction(double val1, double val2)
+        {
+            return val1 - val2;
+        }
+
+        private static double Multiplication(double val1, double val2)
+        {
+            return val1 * val2;
+        }
+
+        private static double Division(double val1, double val2)
+        {
+            return val1 / val2;
+        }
+        static void Main(string[] args)
+        {
+            double value1, value2;
+            string action;
+
+            Console.WriteLine("Введите превое число: ");
+            value1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число: ");
+            value2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию: +, -, *, /");
+            action = Console.ReadLine();
+
+            switch(action)
+            {
+                case "+":
+                    Console.WriteLine($"Результат: {value1} + {value2} = " + Sum(value1, value2));
+                    break;
+                case "-":
+                    Console.WriteLine($"Результат: {value1} - {value2} = " + Subtraction(value1, value2));
+                    break;
+                case "*":
+                    Console.WriteLine($"Результат: {value1} * {value2} = " + Multiplication(value1, value2));
+                    break;
+                case "/":
+                    if (value2 == 0)
+                    {
+                        Console.WriteLine("На 0 делить нельзя");
+                        break;
+                    }
+                    Console.WriteLine($"Результат: {value1} / {value2} = " + Division(value1, value2));
+                    break;
+                default:
+                    Console.WriteLine("Укажите корректную операцию");
+                    break;
+            }
+        }
+    }
+}
